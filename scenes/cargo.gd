be2eca5c4743d62dbc29a82b973c83208b067273
Seth Louis <sethvihanlouis@gmail.com>
2023-07-16 06:28:31 -0400

    added escape

--- scenes/cargo.gd
+++ scenes/cargo.gd
@@ -12,5 +12,6 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 	else: selected = true
 
 func _physics_process(delta: float) -> void:
+	move_and_slide()
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
