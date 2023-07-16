ca59877317e170d7028dc96019ad68843b291164
Seth Louis <sethvihanlouis@gmail.com>
2023-07-16 06:43:29 -0400

    added basic physics

--- scenes/cargo.gd
+++ scenes/cargo.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 var selected = false
 
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -13,5 +13,10 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	# gravity logic
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# pick up logic
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
