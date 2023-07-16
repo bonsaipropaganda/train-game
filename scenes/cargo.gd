1659eb7ac0312590ea7198023076cf210e27dcc7
Seth Louis <sethvihanlouis@gmail.com>
2023-07-16 06:24:08 -0400

    added draggable boxes

---
+++ scenes/cargo.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+var selected = false
+
+
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !Input.is_action_pressed("mb_left"):
+		selected = false
+	else: selected = true
+
+func _physics_process(delta: float) -> void:
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
